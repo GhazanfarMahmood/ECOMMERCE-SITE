09dc2ac8e6cb8c9c623097a6b6e1da33238dfa18
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-03-21 16:45:56 +0500

    adding colors to categories

--- src/components/Products/ProductCateogries/ProductCateogries.jsx
+++ src/components/Products/ProductCateogries/ProductCateogries.jsx
@@ -1,43 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
-//ProductCategories CSS
-import './ProductCategories.scss'
+// ProductCategories CSS
+import "./ProductCategories.scss";
 
 import { TbCategoryPlus } from "react-icons/tb";
-import { ProductConsumer } from '../../../Context/ContextProvider';
-import ProductCom from '../../../common/ProductCom/ProductCom';
+import { ProductConsumer } from "../../../Context/ContextProvider";
+import ProductCom from "../../../common/ProductCom/ProductCom";
 
-const ProductCateogries = () => {
-  const AllProduct = ProductConsumer();
+const ProductCategories = () => {
+  const allProducts = ProductConsumer();
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-  
-  const WomenCategory = () =>{
-   
-    
-  
-     
-      // console.log(AllProduct.map((product) =>(
-      //   product.category."electronics";
-      // )));
-  
-  }
+  const productByCategories = (category) => {
+    if (!category) return allProducts;
+
+    return allProducts.filter((product) => product.category === category);
+  };
+
+  const handleCategoryClick = (category) => {
+    setSelectedProduct(category);
+  };
+
+  const filteredProducts = selectedProduct
+    ? productByCategories(selectedProduct)
+    : allProducts;
 
   return (
-    <>
     <div className="product-cate-container">
-
       <div className="product-cate-card">
-      <h3>Categories <TbCategoryPlus /></h3> 
-      <button >All</button>
-      <button>Men's Category</button>
-      <button onClick={WomenCategory}>Women's Category</button>
-      <button>Jewelery</button>
-      <button>Electronics</button>
+        <h3>
+          Categories <TbCategoryPlus />
+        </h3>
+        <button
+          onClick={() => handleCategoryClick(null)}
+          className={!selectedProduct && "newClass"}
+        >
+          All
+        </button>
+        <button
+          onClick={() => handleCategoryClick("men's clothing")}
+          className={selectedProduct === "men's clothing" && "newClass"}
+        >
+          Men's Category
+        </button>
+        <button
+          onClick={() => handleCategoryClick("women's clothing")}
+          className={selectedProduct === "women's clothing" && "newClass"}
+        >
+          Women's Category
+        </button>
+        <button
+          onClick={() => handleCategoryClick("jewelery")}
+          className={selectedProduct === "jewelery" && "newClass"}
+        >
+          Jewelry
+        </button>
+        <button
+          onClick={() => handleCategoryClick("electronics")}
+          className={selectedProduct === "electronics" && "newClass"}
+        >
+          Electronics
+        </button>
       </div>
-      <ProductCom data={AllProduct} />
+      <ProductCom data={filteredProducts} />
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default ProductCateogries
+export default ProductCategories;
