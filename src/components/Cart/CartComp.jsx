4da1960a1dc2874e82e5ea387893ca22bf8ee1e0
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-04-03 18:13:02 +0500

    Adding functionality to cart page

--- src/components/Cart/CartComp.jsx
+++ src/components/Cart/CartComp.jsx
@@ -1,14 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from "react";
 
-import './Cart.scss'
+import { MdDelete } from "react-icons/md";
 
-const CartComp = ({data}) => {
-  
+import "./Cart.scss";
+
+const CartComp = ({ data }) => {
+  const [increment, setIncrement] = useState(0);
+  const increaseValue = () => {
+    setIncrement(increment + 1);
+  };
+  const decreaseValue = () => {
+    setIncrement(increment - 1);
+  };
   return (
-    <div>
-      <h1>{data.title}</h1>
+    <div className="cart-comp-container">
+      <img src={data.image} alt={data.title} />
+      <div className="cart-text">
+        <div>
+        <h3>
+          Name: <span>{data.title}</span>
+        </h3>
+        <h3>
+          Price:<span> $ {Math.floor(data.price)}</span>
+        </h3>
+        </div>
+        <div className="input-value">
+        <button onClick={increaseValue}>+</button>
+        <input type="text" value={increment} />
+        <button onClick={decreaseValue}>-</button>
+        </div>
+        <span className="delete-btn">
+         Delete: <MdDelete />
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartComp
+export default CartComp;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,28 +1,34 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from "react";
 
 //CrumbLink component
-import BreadCrumbLink from '../../common/BreadCrumbLink/BreadCrumbLink'
-import { ProductContext } from '../../Context/ContextProvider'
-import CartComp from '../../components/Cart/CartComp';
-import CartShow from '../../components/Cart/CartShow';
+import BreadCrumbLink from "../../common/BreadCrumbLink/BreadCrumbLink";
+import { ProductContext } from "../../Context/ContextProvider";
+import CartComp from "../../components/Cart/CartComp";
+import CartShow from "../../components/Cart/CartShow";
+import CartPrice from "../../components/Cart/CartPrice";
 
 const Cart = () => {
-  const {cartItems }=  useContext(ProductContext);
- 
+  const { cartItems } = useContext(ProductContext);
+
   return (
     <>
-    <div className='ms-3'>
-      <BreadCrumbLink pageName={"Cart"} />
-      {cartItems.length > 0 ? (
-            cartItems.map((product) => {
-              return (
-                <CartComp data={product} key={product.id} />
-              )
-            } )
-            ): (<CartShow />)}
+      <div className="ms-3" >
+        <BreadCrumbLink pageName={"Cart"} />
+        {cartItems.length > 0 ? (
+          <div className="cart-main-container">
+          <div className="width">
+            {cartItems.map((product) => {
+              return <CartComp data={product} key={product.id} />;
+            })}
+          </div>
+            <CartPrice />
+            </div>
+        ) : (
+          <CartShow />
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
